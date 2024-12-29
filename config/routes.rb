28276729bec59514a0bebc6f91596bdde7540e3e
n7n788 <yuki1113.2001@gmail.com>
2024-12-29 19:42:14 +0900

    add: StaticPageでのホームとヘルプページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'application#hello'
+  get 'static_pages/home'
+  get 'static_pages/help'
 end
