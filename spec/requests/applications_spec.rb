956129418ef84abbbc3531807f82cb23354b9d32
n7n788 <yuki1113.2001@gmail.com>
2024-12-31 20:36:43 +0900

    fix: ルーティングを変更

--- spec/requests/applications_spec.rb
+++ spec/requests/applications_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Applications", type: :request do
-  describe '#hello' do
-    it 'returns a http success' do
-      get root_path
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,28 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   describe '#home' do
     it 'returns a http success' do
-      get static_pages_home_path
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe '#help' do
     it 'returns a http success' do
-      get static_pages_help_path
+      get help_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#contact' do
+    it 'return a http success' do
+      get contact_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#about' do
+    it 'returns a http success' do
+      get about_path
       expect(response).to have_http_status(:success)
     end
   end
