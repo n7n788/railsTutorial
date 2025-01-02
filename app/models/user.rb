1db91b058e7966675da88b1427f419ee1aa95e8b
n7n788 <yuki1113.2001@gmail.com>
2025-01-02 19:08:02 +0900

    add: ユーザモデルにパスワードを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
-
+  has_secure_password
 end
